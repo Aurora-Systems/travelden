89aa2f9c60642b44411683d0ca4913e24cef6626
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2025-03-31 12:56:19 +0200

    Refactor form inputs in Book page for better accessibility and data handling

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,7 +1,29 @@
+"use client"
 import { Fade } from "react-awesome-reveal"
 import Image from "next/image"
+import { FormEvent } from "react"
+import emailjs from "@emailjs/browser"
+import { useState, useRef } from "react"
 
 export default function Book(){
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const form:any = useRef(null)
+    const [loading,set_loading] = useState<boolean>(false)
+
+    const send_application = (e: FormEvent) => {
+        e.preventDefault()
+        set_loading(true)
+        emailjs.sendForm("service_xwf6fms", "template_c4pjfnx", form.current, {
+            publicKey: "CLt7ZGjfiSQEopImB"
+        }).then(() => {
+            alert("✅ We received your enquiry, expect a call or an email soon!")
+            form.current.reset()
+        }).catch(() => {
+            alert("⚠️ Message not sent, please try again or send us a message on our email support@aurorasystems.co.zw!")
+        }).finally(()=>{
+            set_loading(false)
+        })
+    }
     return(
         <div>
             <div className="min-vh-100 primary pt-5 pb-5" >
@@ -13,60 +35,69 @@ export default function Book(){
                 </div>
                 <p className="text-center text-white fw-bold">Fill up the form and we will find and plan the best trip for you</p>
                 <div className="rounded container bg-white p-3">
-                    <form>
+                    <form onSubmit={send_application} ref={form}>
                         <div className="row">
                             <div className="col-sm mb-2"> 
                                 <span>Full Name</span>
-                                <input type="text" className="form-control" />
+                                <input type="text" name="full_name" className="form-control" />
                             </div>
                             <div className="col-sm mb-2"> 
                                 <span>Email</span>
-                                <input type="text" className="form-control" />
+                                <input type="text" name="email" className="form-control" />
                             </div>
                             <div className="col-sm mb-2"> 
                                 <span>Contact Number</span>
-                                <input type="text" className="form-control" />
+                                <input type="text" name="contact_number" className="form-control" />
                             </div>
                         </div>
                         <div className="row">
                         <div className="col-sm mb-2"> 
                                 <span>For Business</span>
-                                <select className="form-control">
+                                <select className="form-control" name="for_business">
                                     <option></option>
-                                    <option>Yes</option>
-                                    <option>No</option>
+                                    <option value="Yes">Yes</option>
+                                    <option value="No">No</option>
 
                                 </select>
                             </div>
                             <div className="col-sm mb-2"> 
                                 <span>Number of Adults</span>
-                                <input type="number" className="form-control" />
+                                <input type="number" name="number_of_adults" className="form-control" />
                             </div>
                             <div className="col-sm mb-2"> 
                                 <span>Number of Kids</span>
-                                <input type="number" className="form-control" />
+                                <input type="number" name="number_of_kids" className="form-control" />
                             </div>
                         </div>
                         <div className="row">
                         <div className="col-sm mb-2"> 
                                 <span>Duration - From</span>
-                                <input type="number" className="form-control" />
+                                <input type="number" name="start_date" className="form-control" />
 
                             </div>
                             <div className="col-sm mb-2"> 
                                 <span>Duration - To</span>
-                                <input type="number" className="form-control" />
+                                <input type="number" name="end_date" className="form-control" />
                             </div>
                             <div className="col-sm mb-2"> 
                                 <span>Budget</span>
-                                <input type="number" className="form-control" />
+                                <input type="number" name="budget" className="form-control" />
                             </div>
                         </div>
                         <div className="mb-2">
-                            <textarea className="form-control"/>
+                            <span>Additional Information</span>
+                            <textarea className="form-control" name="additional_information"/>
                         </div>
                         <div className="mb-2">
-                            <button className="p-btn btn rounded-pill">Send</button>
+                            <span>Where Are you located</span>
+                            <select className="form-control">
+                                <option value="Zimbabwe">Zimbabwe</option>
+                                <option value="Zambia">Zambia</option>
+                                <option value="Other country">Other Country</option>
+                            </select>
+                        </div>
+                        <div className="mb-2">
+                            <button className="p-btn btn rounded-pill" type="submit" disabled={loading}>{loading?"Sending":"Send"}</button>
                         </div>
 
                     </form>
