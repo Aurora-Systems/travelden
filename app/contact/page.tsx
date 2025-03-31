8c6e7c157ca4c7baf88fb8fddebb10b484034946
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2025-03-31 13:00:54 +0200

    Implement contact form with email sending functionality and loading state

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,25 @@
+import { FormEvent, useRef, useState } from "react"
+import emailjs from "@emailjs/browser"
+
 export default function Contact(){
+     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const form:any = useRef(null)
+        const [loading,set_loading] = useState<boolean>(false)
+    
+        const send_application = (e: FormEvent) => {
+            e.preventDefault()
+            set_loading(true)
+            emailjs.sendForm("service_xwf6fms", "template_c4pjfnx", form.current, {
+                publicKey: "CLt7ZGjfiSQEopImB"
+            }).then(() => {
+                alert("✅ We received your enquiry, expect a call or an email soon!")
+                form.current.reset()
+            }).catch(() => {
+                alert("⚠️ Message not sent, please try again or send us a message on our email support@aurorasystems.co.zw!")
+            }).finally(()=>{
+                set_loading(false)
+            })
+        }
     return(
         <div className="container  mb-2">
             <div className="text-center">
@@ -13,25 +34,25 @@ export default function Contact(){
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d576.471221353393!2d31.004090992309084!3d-17.82685750114293!2m3!1f12.459499263622943!2f0!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x1931a5c920c6e1f3%3A0x87acf3aca622dc36!2sLongcheng%20Plaza!5e1!3m2!1sen!2szw!4v1743077006281!5m2!1sen!2szw" className="rounded" height="100%"  width="100%" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                 <div className="col-sm">
-                    <form>
+                    <form onSubmit={send_application} ref={form}>
                         <div className="mb-2">
                             <span>Name</span>
-                            <input type="text" className="form-control"/>
+                            <input type="text" name="full_name" className="form-control"/>
                         </div>
                         <div className="mb-2">
                             <span>Contact Number</span>
-                            <input type="text" className="form-control"/>
+                            <input type="text" name="contact_number" className="form-control"/>
                         </div>
                         <div className="mb-2">
                             <span>Email</span>
-                            <input type="text" className="form-control"/>
+                            <input type="text" name="email" className="form-control"/>
                         </div>
                         <div className="mb-2">
                             <span>Message</span>
-                            <textarea className="form-control" />
+                            <textarea name="message" className="form-control" />
                         </div>
                         <div className="mb-2">
-                            <button className="btn rounded-pill p-btn">Send</button>
+                            <button className="btn rounded-pill p-btn" type="submit">{loading?"Sending":"Send"}</button>
                         </div>
                     </form>
                 </div>
